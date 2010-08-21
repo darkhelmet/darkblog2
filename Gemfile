abfea160691fd25451e6f49d430bd59b90a31902
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2010-08-20 23:15:25 -0600

    ditch prototype for jquery. mongo help

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.0.rc'
-gem 'pg', '0.9.0' # Talk to the DB
+gem 'rails3-generators'
 gem 'haml', '3.0.17' # ERB sucks
 gem 'yajl-ruby', '0.7.7', :require => 'yajl' # JSON
 gem 'mongo_mapper', '0.8.3'
@@ -10,10 +10,8 @@ gem 'bson_ext'
 gem 'hoptoad_notifier'
 gem 'memcached-northscale', '0.19.5.3', :require => 'memcached'
 
-group :development do
+group :development, :test do
   gem 'ruby-debug'
-end
-
-group :test do
   gem 'rspec-rails', '2.0.0.beta.19'
+  gem 'factory_girl'
 end
--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Darkblog2
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Mountain Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -40,5 +40,11 @@ module Darkblog2
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    config.generators do |g|
+      g.orm :mongo_mapper
+      g.template_engine :haml
+      g.test_framework :rspec, :fixture_replacement => :factory_girl
+    end
   end
 end
