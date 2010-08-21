37fee5106c7cff2a4980a3ba5b40b7c952ef39c9
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2010-08-20 20:57:25 -0600

    initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0.rc'
+gem 'pg', '0.9.0' # Talk to the DB
+gem 'haml', '3.0.17' # ERB sucks
+
+group :development do
+  gem 'ruby-debug'
+end
+
+group :test do
+  gem 'rspec-rails', '2.0.0.beta.19'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Darkblog2::Application.config.secret_token = 'acc3dd9582237c271c92b7d6df9c68e39d49fd8a3bb0f0ad1279584a8b837a0bd20f31a8ea5f7c0c876c3ada089a52ffb41c625cc505253cd434b12f6751b48d'
