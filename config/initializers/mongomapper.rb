87c14f428abde771f50cfe247fc3b6486a96cb0a
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2010-08-23 02:11:08 -0600

    Don't set database in production

--- config/initializers/mongomapper.rb
+++ config/initializers/mongomapper.rb
@@ -1,2 +1,6 @@
-MongoMapper.connection = Rails.env.production? ? Mongo::Connection.from_uri(ENV['MONGOHQ_URL']) : Mongo::Connection.new('localhost', 27017, :logger => Rails.logger)
-MongoMapper.database = "darkblog2-#{Rails.env}"
+MongoMapper.connection = if Rails.env.production?
+  Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+else
+  Mongo::Connection.new('localhost', 27017, :logger => Rails.logger)
+  MongoMapper.database = "darkblog2-#{Rails.env}"
+end
