371557203fe002ba20446ce40f0ec2a48daeb8c9
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2010-08-23 02:18:06 -0600

    getting tired...

--- config/initializers/mongomapper.rb
+++ config/initializers/mongomapper.rb
@@ -1,9 +1,9 @@
-MongoMapper.connection = if Rails.env.production?
+if Rails.env.production?
   ENV['MONGOHQ_URL'].tap do |url|
-    Mongo::Connection.from_uri(url)
+    MongoMapper.connection = Mongo::Connection.from_uri(url)
     MongoMapper.database = url.split('/').last
   end
 else
-  Mongo::Connection.new('localhost', 27017, :logger => Rails.logger)
+  MongoMapper.connection = Mongo::Connection.new('localhost', 27017, :logger => Rails.logger)
   MongoMapper.database = "darkblog2-#{Rails.env}"
 end
