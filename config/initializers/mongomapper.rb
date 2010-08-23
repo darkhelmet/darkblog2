45f77005119c40fac5a4792ebf86f2bb8da9b5ba
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2010-08-23 02:14:10 -0600

    Set database to mongohq one

--- config/initializers/mongomapper.rb
+++ config/initializers/mongomapper.rb
@@ -1,5 +1,8 @@
 MongoMapper.connection = if Rails.env.production?
-  Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+  ENV['MONGOHQ_URL'].tap do |url|
+    Mongo::Connection.from_uri()
+    MongoMapper.database = url.split('/').last
+  end
 else
   Mongo::Connection.new('localhost', 27017, :logger => Rails.logger)
   MongoMapper.database = "darkblog2-#{Rails.env}"
