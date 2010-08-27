75b10dff4d3ee4362d4379f14817e6f6a075daf7
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2010-08-26 19:27:22 -0600

    Light refactoring. Meta description tags. Closes #14

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,14 @@
 module ApplicationHelper
+  def jquery_tag(version)
+    javascript_include_tag("http://ajax.googleapis.com/ajax/libs/jquery/#{version}/jquery.min.js")
+  end
+
+  def font_tag(family)
+    stylesheet_link_tag("http://fonts.googleapis.com/css?family=#{family}")
+  end
+
   def yield_or_default(key, default)
-    content_for(key).tap do |text|
-      text << default if text.blank?
-    end
+    content_for?(key) ? content_for(key) : default
   end
 
   def favicon_tag(path)
@@ -11,5 +17,9 @@ module ApplicationHelper
 
   def gravatar(email, size = 120)
     image_tag("http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email.strip.downcase)}.png?s=#{size}", :alt => 'Gravatar for Daniel Huckstep')
+  end
+
+  def description_tag
+    tag(:meta, :name => 'description', :content => yield_or_default(:description, Darkblog2.config[:tagline]))
   end
 end
