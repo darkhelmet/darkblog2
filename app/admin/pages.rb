4c9bcbd620c3cc963132e7b1c2a570484cd109d5
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2011-12-12 20:23:50 -0700

    Better pages admin screen

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -1,3 +1,23 @@
 ActiveAdmin.register Page do
+  index do |post|
+    column :title do |page|
+      link_to(page.title, edit_admin_page_path(page))
+    end
+    column :slug
+    column 'Actions' do |page|
+      ul do
+        li link_to('View', page_path(page.slug))
+        li link_to('Edit', edit_admin_page_path(page))
+        li link_to('Delete', admin_page_path(page), method: :delete, confirm: 'Really delete?')
+      end
+    end
+  end
+
   form partial: 'form'
+
+  sidebar :help do
+    ul do
+      li link_to('Textile Reference', 'http://redcloth.org/hobix.com/textile/')
+    end
+  end
 end
