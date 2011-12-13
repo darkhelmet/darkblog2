46d04609137773935ec75b3fe295ab1f779200cc
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2011-12-12 20:24:18 -0700

    Updated routes for pages and 1.9 syntax

--- config/routes.rb
+++ config/routes.rb
@@ -3,33 +3,27 @@ Darkblog2::Application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
-  root :to => 'posts#main'
-  get '/opensearch.xml' => 'application#opensearch', :as => :opensearch, :format => 'xml'
-  get '/search(.:format)' => 'posts#search', :as => :search
-  get '/feed' => 'posts#feed', :as => :feed, :format => 'xml'
-  get '/sitemap.:format' => 'posts#sitemap', :as => :sitemap, :format => 'xml', :constraints => {
-    :format => /xml(\.gz)?/
+  root to: 'posts#main'
+  get '/opensearch.xml' => 'application#opensearch', as: :opensearch, format: :xml
+  get '/search(.:format)' => 'posts#search', as: :search
+  get '/feed' => 'posts#feed', as: :feed, format: :xml
+  get '/sitemap.:format' => 'posts#sitemap', as: :sitemap, format: :xml, constraints: {
+    format: /xml(\.gz)?/
   }
-  get '/archive/:archive' => 'posts#archive', :as => :archive, :constraints => {
-    :archive => /full|category|month/
+  get '/archive/:archive' => 'posts#archive', as: :archive, constraints: {
+    archive: /full|category|month/
   }
-  get '/:year/:month(.:format)' => 'posts#monthly', :as => :monthly, :constraints => {
-    :year => /\d{4}/,
-    :month => /\d{2}/
+  get '/:year/:month(.:format)' => 'posts#monthly', as: :monthly, constraints: {
+    year: /\d{4}/,
+    month: /\d{2}/
   }
-  get '/category/:category(.:format)' => 'posts#category', :as => :category
-  get '/:year/:month/:day/:slug(.:format)' => 'posts#permalink', :as => :permalink, :constraints => {
-    :year => /\d{4}/,
-    :month => /\d{2}/,
-    :day => /\d{2}/
+  get '/category/:category(.:format)' => 'posts#category', as: :category
+  get '/:year/:month/:day/:slug(.:format)' => 'posts#permalink', as: :permalink, constraints: {
+    year: /\d{4}/,
+    month: /\d{2}/,
+    day: /\d{2}/
   }
-  get '/tag/:tag(.:format)' => 'posts#tag', :as => :tag
-
-  Dir[Rails.root.join('app', 'views', 'pages', '*')].map do |path|
-    path.split('/').last.split('.').first
-  end.join('|').tap do |pages|
-    get '/:page' => 'static#page', :as => :page, :constraints => { :page => Regexp.new(pages) }
-  end
-
+  get '/tag/:tag(.:format)' => 'posts#tag', as: :tag
+  get '/:page' => 'pages#show', as: :page
   get '/*not_found' => 'application#render_404'
 end
