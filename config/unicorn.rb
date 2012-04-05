615cc0067c272178a4be7919585126b31d227e4a
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2012-04-04 20:06:52 -0600

    disconnect before fork

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,6 +3,10 @@ worker_processes 4
 preload_app true
 timeout 30
 
+before_fork do |server, worker|
+  ActiveRecord::Base.connection.disconnect!
+end
+
 after_fork do |server, worker|
   ActiveRecord::Base.establish_connection
   Rails.cache.reset if Rails.cache.respond_to?(:reconnect)
